2d7d52b62bd856da8e06d70879bbcd1faba68fc4
iqfaddi <iqfadia23@gmail.com>
2025-12-28 01:05:01 +0200

    Update start.py

--- start.py
+++ start.py
@@ -1,7 +1,6 @@
 import asyncio, os
 import uvicorn
 from webapp import create_app
-from bot_admin import run_polling
 from bot_stock import run_stock_bot
 
 async def main():
@@ -9,7 +8,11 @@ async def main():
     port = int(os.getenv("PORT", "8000"))
     config = uvicorn.Config(app, host="0.0.0.0", port=port)
     server = uvicorn.Server(config)
-    await asyncio.gather(server.serve(), run_polling(), run_stock_bot())
+
+    await asyncio.gather(
+        server.serve(),
+        run_stock_bot()
+    )
 
 if __name__ == "__main__":
     asyncio.run(main())
